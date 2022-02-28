67c1ddd1b31ee6a7ad1e24327febce133e7089e8
130 <130@130-2>
2022-02-28 13:47:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static public void Main()
+        {
+            Student st = new Student(3, 5, "Иванов");
+            st.Show();
+            st.Avarage();
+            Console.ReadKey();
+        }
+
+
+        class Student
+        {
+            string FIO;
+            int ocenka1;
+            int ocenka2;
+            public Student(int oc1, int oc2, string fio)
+            {
+                this.FIO = fio;
+                ocenka1 = oc1;
+                ocenka2 = oc2;
+            }
+            public void Show()
+            {
+                Console.WriteLine("Фамилия: " + FIO + "     оценка1: " + ocenka1 + "    оценка2: " + ocenka2);
+            }
+            public void Avarage()
+            {
+
+                Console.WriteLine("Cредняя оценка = " + ((ocenka2 + ocenka1) / 2));
+            }
+        }
+    }
+}
